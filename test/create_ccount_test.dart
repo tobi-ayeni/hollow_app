5e65ff951f7f2bca0d248b749046a9b07178cdbe
tobi-ayeni <rotimiayenioluwatobiloba@gmail.com>
2022-05-27 13:51:04 +0100

    added widget test

--- test/create_ccount_test.dart
+++ test/create_ccount_test.dart
@@ -17,9 +17,12 @@ void main() {
   testWidgets("Check widgets by key", (WidgetTester tester) async {
     await tester.pumpWidget(app);
     expect(find.byKey(const Key("scrollView")), findsOneWidget);
-    expect(find.byKey(const Key("scrollView")), findsOneWidget);
-    expect(find.byKey(const Key("scrollView")), findsOneWidget);
-    expect(find.byKey(const Key("scrollView")), findsOneWidget);
+    expect(find.byKey(const Key("lastNameKey")), findsOneWidget);
+    expect(find.byKey(const Key("nameKey")), findsOneWidget);
+    expect(find.byKey(const Key("userNameKey")), findsOneWidget);
+    expect(find.byKey(const Key("phoneKey")), findsOneWidget);
+    expect(find.byKey(const Key("emailKey")), findsOneWidget);
+    expect(find.byKey(const Key("passwordKey")), findsOneWidget);
 
     await tester.pump();
   });
